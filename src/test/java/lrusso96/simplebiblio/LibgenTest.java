20bdcab5ab123b024bc85e0435f056d50e890777
Luigi <russo.1699981@studenti.uniroma1.it>
2019-10-08 16:10:09 +0200

    some test for libgen

---
+++ src/test/java/lrusso96/simplebiblio/LibgenTest.java
@@ -0,0 +1,29 @@
+package lrusso96.simplebiblio;
+
+import lrusso96.simplebiblio.core.Ebook;
+import lrusso96.simplebiblio.core.providers.feedbooks.Feedbooks;
+import lrusso96.simplebiblio.core.providers.feedbooks.FeedbooksBuilder;
+import lrusso96.simplebiblio.core.providers.feedbooks.Language;
+import lrusso96.simplebiblio.core.providers.libgen.LibraryGenesis;
+import lrusso96.simplebiblio.core.providers.libgen.LibraryGenesisBuilder;
+import lrusso96.simplebiblio.exceptions.BiblioException;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class LibgenTest {
+    @Test
+    public void simpleSearch() throws BiblioException {
+        LibraryGenesis libgen = new LibraryGenesisBuilder().build();
+        List<Ebook> ret = libgen.search("Carroll");
+        assertNotEquals(0, ret.size());
+        Ebook book = ret.get(0);
+        assertNotNull(book.getAuthor());
+        assertNotEquals(0, book.getId());
+        assertNotNull(book.getTitle());
+        libgen.loadDownloadURI(book);
+        assertNotNull(book.getDownload());
+    }
+}
