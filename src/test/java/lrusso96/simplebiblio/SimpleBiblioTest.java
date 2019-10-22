bec63a616fb66a7d8027ebd800aaff16754e6213
Luigi <russo.1699981@studenti.uniroma1.it>
2019-10-22 19:41:06 +0200

    add test for biblio methods

--- src/test/java/lrusso96/simplebiblio/SimpleBiblioTest.java
+++ src/test/java/lrusso96/simplebiblio/SimpleBiblioTest.java
@@ -12,14 +12,37 @@ import static org.junit.Assert.assertNotNull;
 
 public class SimpleBiblioTest {
     @Test
-    public void simpleSearchTest() {
+    public void simpleSearchTest() throws InterruptedException {
         SimpleBiblio biblio = new SimpleBiblioBuilder().build();
         List<Ebook> ret = biblio.searchAll("Carroll");
+        simpleTest(ret);
+    }
+
+    @Test
+    public void getRecentTest() {
+        SimpleBiblio biblio = new SimpleBiblioBuilder().build();
+        List<Ebook> ret = biblio.getAllRecent();
+        simpleTest(ret);
+    }
+
+    @Test
+    public void getPopularTest() {
+        SimpleBiblio biblio = new SimpleBiblioBuilder().build();
+        List<Ebook> ret = biblio.getAllPopular();
+        simpleTest(ret);
+    }
+
+    private void simpleTest(List<Ebook> ret) {
         assertNotEquals(0, ret.size());
         Ebook book = ret.get(0);
         assertNotNull(book.getAuthor());
         assertNotEquals(0, book.getId());
         assertNotNull(book.getTitle());
         assertNotNull(book.getDownload());
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
