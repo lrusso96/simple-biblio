d9f1a7ea753208277c21109aac4a4a738160c94b
Luigi <russo.1699981@studenti.uniroma1.it>
2020-02-15 18:45:54 +0100

    Provider: migrate to kotlin

---
+++ src/main/java/lrusso96/simplebiblio/core/Provider.kt
@@ -0,0 +1,59 @@
+package lrusso96.simplebiblio.core
+
+import lrusso96.simplebiblio.exceptions.BiblioException
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+abstract class Provider(val name: String, private val maxTries: Int = 3) {
+
+    suspend fun search(query: String): List<Ebook> {
+        var cnt = 0
+        while (true) try {
+            return doSearch(query)
+        } catch (ex: BiblioException) {
+            if (++cnt == maxTries) {
+                logger.error { "$name failed to retrieve result for $maxTries time(s)" }
+                return ArrayList()
+            }
+        }
+    }
+
+    @Throws(BiblioException::class)
+    protected abstract suspend fun doSearch(query: String): List<Ebook>
+
+    suspend fun getRecent(): List<Ebook> {
+        var cnt = 0
+        while (true) try {
+            return doGetRecent()
+        } catch (ex: BiblioException) {
+            if (++cnt == maxTries) {
+                logger.error { "$name failed to retrieve result for $maxTries time(s)" }
+                return ArrayList()
+            }
+        }
+    }
+
+    @Throws(BiblioException::class)
+    protected abstract suspend fun doGetRecent(): List<Ebook>
+
+    suspend fun getPopular(): List<Ebook> {
+        var cnt = 0
+        while (true) try {
+            return doGetPopular()
+        } catch (ex: BiblioException) {
+            if (++cnt == maxTries) {
+                logger.error { "$name failed to retrieve result for $maxTries time(s)" }
+                return ArrayList()
+            }
+        }
+    }
+
+    @Throws(BiblioException::class)
+    protected abstract suspend fun doGetPopular(): List<Ebook>
+
+    companion object {
+        const val LIBGEN = "Library Genesis"
+        const val FEEDBOOKS = "Feedbooks"
+        const val STANDARD_EBOOKS = "Standard Ebooks"
+    }
+}
